0fe613c154f245892ddfa44b21603f7caa2910a3
gaoboai1314 <419045177@qq.com>
2017-11-27 01:48:54 +0800

    处理动态数据，渲染页面，实现点击事件基本功能，并封装成组件

--- src/App.js
+++ src/App.js
@@ -4,11 +4,96 @@ import './assets/css/App.css';
 import Tree from './component/tree';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    console.log(props)
+    this.state={
+      title: "招聘职位",
+      data: []
+    };
+  } ;
+  componentDidMount () {
+    let data = [
+      {
+        value: 0,
+        label: '工程研发部门',
+        count: 120,
+        checked: false,
+        children: [
+          {
+            value: 0,
+            label: 'Mac开发工程师',
+            count: 9,
+            checked: false
+          },
+          {
+            value: 1,
+            label: 'iOS App测试工程师',
+            count: 17,
+            checked: false
+          },
+          {
+            value: 2,
+            label: 'Android远程控制工程师',
+            count: 61,
+            checked: false
+          },
+          {
+            value: 3,
+            label: 'Web 前端工程师',
+            count: 31,
+            checked: false
+          },
+          {
+            value: 4,
+            label: 'Android 多媒体软件开发工程师',
+            count: 2,
+            checked: false
+          }
+        ]
+      },
+      {
+        value: 1,
+        label: '产品设计部门',
+        count: 136,
+        checked: false,
+        children: [
+          {
+            value: 0,
+            label: '网页设计师',
+            count: 47,
+            checked: false
+          },
+          {
+            value: 1,
+            label: 'ID/工业设计师',
+            count: 39,
+            checked: false
+          },
+          {
+            value: 2,
+            label: '视觉设计师/GUI界面设计师',
+            count: 42,
+            checked: false
+          },
+          {
+            value: 3,
+            label: '平面设计师',
+            count: 8,
+            checked: false
+          }
+        ]
+      }
+    ]
+    // setTimeout(() => {
+      this.setState({data: data})
+    // }, 2000)
+  }
   render() {
     return (
       <div className="App">
         <aside className="list">
-          <Tree />
+          <Tree title={this.state.title} data={this.state.data}/>
         </aside>
       </div>
     );
--- src/component/tree.js
+++ src/component/tree.js
@@ -2,33 +2,96 @@ import React, { Component } from 'react';
 import '../assets/css/tree.css';
 
 class Tree extends Component {
+	constructor(props){
+			super(props)
+			this.state={
+				data: []
+			}
+	} 
+	componentWillReceiveProps  (nextProps) {
+		this.setState({
+			data: nextProps.data
+		})
+	}
+	clearHandleClick () {
+		var myData = this.state.data
+		for (let i = 0; i < myData.length; i++){
+			myData[i].checked = false
+			if (myData[i].children) {
+				for (let j = 0; j < myData[i].children.length; j++) {
+					myData[i].children[j].checked = false
+				}
+			}
+			this.setState({
+				data: myData
+			})
+		}
+	}
+	itemHandleClick (index, event) {
+		var myData = this.state.data
+		if (!myData[index[0]]) return
+		if (index.length === 1) {
+			myData[index[0]].checked = !myData[index[0]].checked
+			if (myData[index[0]].children) {
+				for (let j = 0; j < myData[index[0]].children.length; j++) {
+					myData[index[0]].children[j].checked = myData[index[0]].checked
+				}
+			}
+		} else {
+			myData[index[0]].children[index[1]].checked = !myData[index[0]].children[index[1]].checked
+			var flag = true
+			for (let i = 0; i < myData[index[0]].children.length; i++) {
+				flag = myData[index[0]].children[i].checked && flag
+			}
+			myData[index[0]].checked = flag
+		}
+		this.setState({
+			data: myData
+		})
+	}
+	handelChange (index, event) {
+		this.itemHandleClick(index, event)
+	}
   render() {
     return (
       <div className="tree">
 				<div className="tree-header clearfix">
-					<div className="title">招聘职位</div>
-					<div className="clear">清空</div>
+					<div className="title">{this.props.title?this.props.title:'列表'}</div>
+					<div className="clear" onClick={this.clearHandleClick.bind(this)}>清空</div>
 				</div>
-        <ul className="tree-list clearfix">
-					<li>
-						<div className="tree-item"><input type="checkbox" />工程研发部门<span className="drop"></span><div className="value">120</div></div>
-						<ul>
-							<li><div className="tree-item"><input type="checkbox" />Mac开发工程师<div className="value">120</div></div></li>
-							<li><div className="tree-item"><input type="checkbox" />iOS App测试工程师<div className="value">120</div></div></li>
-							<li><div className="tree-item"><input type="checkbox" />Android远程控制工程师<div className="value">120</div></div></li>
-							<li><div className="tree-item"><input type="checkbox" />Web 前端工程师<div className="value">120</div></div></li>
-							<li><div className="tree-item"><input type="checkbox" />Android 多媒体软件开发工程师<div className="value">120</div></div></li>
-						</ul>
-					</li>
-					<li>
-						<div className="tree-item"><input type="checkbox" />产品设计部门<span className="drop"></span><div className="value">9</div></div>
-						<ul>
-							<li><div className="tree-item"><input type="checkbox" />网页设计师<div className="value">120</div></div></li>
-							<li><div className="tree-item"><input type="checkbox" />ID/工业设计师<div className="value">120</div></div></li>
-							<li><div className="tree-item"><input type="checkbox" />视觉设计师/GUI界面设计师<div className="value">9</div></div></li>
-							<li><div className="tree-item"><input type="checkbox" />平面设计师<div className="value">120</div></div></li>
-						</ul>
-					</li>
+				<ul className="tree-list clearfix">
+					{
+						(this.state.data && typeof this.props.data === 'object' && this.state.data.length) ?
+							this.state.data.map((parent, index) => {
+								return <li key={parent.value}>
+												<div className="tree-item">
+													<input type="checkbox" onChange={this.itemHandleClick.bind(this, [index])} checked={parent.checked} />
+													<span onClick={this.itemHandleClick.bind(this, [index])}>{parent.label}</span>
+													{
+														(parent.children && parent.children.length)?
+														<span className="drop"></span> : null
+													}
+													<div className="value">{parent.count}</div>
+												</div>
+												{
+													(parent.children && parent.children.length)?
+													<ul>
+														{
+															parent.children.map((children, i) => {
+																return <li key={children.value}>
+																				<div className="tree-item">
+																					<input type="checkbox" onClick={this.itemHandleClick.bind(this, [index, i])} checked={children.checked}/>
+																					<span onClick={this.itemHandleClick.bind(this, [index, i])}>{children.label}</span>
+																					<div className="value">{children.count}</div>
+																				</div>
+																			</li>
+															})
+														}
+													</ul> : null
+												}
+											</li>
+							}) : <li>请传入参数</li>
+					}
 				</ul>
       </div>
     );
